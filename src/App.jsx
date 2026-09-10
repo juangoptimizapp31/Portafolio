0a20005a2d4aaf89c65bce9fc68bcc5e7568c10d
juangoptimizapp31 <juangoptimizapp@gmail.com>
2026-09-10 10:56:41 -0500

    Fix import statement for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/Footer'; 
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Certifications from './pages/Certifications';
